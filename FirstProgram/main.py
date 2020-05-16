2a4e2250edf33a6fd4e2ab4375e7e26d852306f9
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-17 00:08:49 +0200

    function and infinite arguments

--- FirstProgram/main.py
+++ FirstProgram/main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+# Functions ( recommanded to use snake case to define a function)
+# In python3 whe use : instead of curly braces {}
+def my_function(name='Kevin', age='22'):
+    print('my name is ' + name + ' I have ' + age)
+
+
+my_function('arsene')
+
+
+#Infinite arguments
+def print_people(*people):
+    for person in people:
+        print('This person is', person)
+
+
+print_people('Miley','Grant','Barnabe','Rugal')
