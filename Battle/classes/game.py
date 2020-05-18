159d4c0fa004ae82260f0c233ca49b1214f00b7d
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-18 12:31:30 +0200

    Additional methods utility

--- Battle/classes/game.py
+++ Battle/classes/game.py
@@ -1,5 +1,6 @@
 import random
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[95m'
@@ -12,7 +13,7 @@ class bcolors:
 
 
 class Person:
-    def __init__(self, hp, mp, atk,df, magic):
+    def __init__(self, hp, mp, atk, df, magic):
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -21,15 +22,33 @@ class Person:
         self.atkh = atk + 10
         self.df = df
         self.magic = magic
-        self.actions = ['attack','magic']
+        self.actions = ['attack', 'magic']
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+
+    def get_max_mp(self):
+        return self.maxmp
 
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
-    def genearete_spell_damage(self,i):
+    def generate_spell_damage(self, i):
         mgl = self.magic[i]['dmg'] - 5
         mgh = self.magic[i]['dmg'] + 5
         return random.randrange(mgl, mgh)
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]["cost"]
 
     def take_damage(self, dmg):
         self.hp -= dmg
@@ -38,3 +57,16 @@ class Person:
 
         return self.hp
 
+    def choose_action(self):
+        i = 1
+        print('Actions :')
+        for item in self.actions:
+            print(str(i), ':', item)
+            i += 1
+
+    def choose_magic(self):
+        i = 1
+        print('Magic :')
+        for item in self.magic:
+            print(str(i), ':', item['name'], " (cost: ", item['cost'], ')')
+            i += 1
