fae5d4ab5c7a127fba169c6efe83ba8fbcc1411e
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-20 11:33:55 +0200

    Writing files in Python (JSON)

--- AdditionalPythonRessources/main.py
+++ AdditionalPythonRessources/main.py
@@ -1,5 +1,17 @@
-newfile = open('newFile.txt','w+')
+import simplejson as json
+import os
 
-string = 'This is the content that will be written to the text file'
+if os.path.isfile('./ages.json') and os.stat('./ages.json').st_size != 0:
+    old_file = open('./ages.json','r+')
+    data = json.loads(old_file.read())
 
-newfile.write(string)
+    print('Current age is', data['age'], '----- adding a year.')
+    data['age'] = data['age'] + 1
+    print('Now age is', data['age'])
+else:
+    old_file = open('./ages.json','w+')
+    data = {'name':'Kevin', 'age': 26}
+    print('No file found setting default age to', data['age'])
+
+old_file.seek(0)
+old_file.write(json.dumps(data))
